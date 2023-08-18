1a9aee96f112a7ffbf645eb8f2e2745cc7375248
ian kamau <35421955+iankamaa70@users.noreply.github.com>
2023-08-18 19:09:16 +0300

    Initial Commit

---
+++ threads/threadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  threadsApp.swift
+//  threads
+//
+//  Created by Allan Njiru on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct threadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
